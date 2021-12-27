eaa51f05fbe4e6f25f9ac33d1aa71bb10053b063
dgnsrekt <dgnsrekt@pm.me>
2021-12-26 23:02:43 -0600

    test: basic profile route testing.

--- api/profile_api/api.py
+++ api/profile_api/api.py
@@ -8,7 +8,7 @@ SPOTIFY_PUBLIC_USER_URL = "https://api.spotify.com/v1/users/{username}"
 router = Router()
 
 
-@router.get("/me", response=schemas.SpotifyPublicProfile)
+@router.get("", response=schemas.SpotifyPublicProfile, url_name="profile")
 def get_users_public_profile_information(request):
     session = spotify.create_spotify_backend_session()
     url = SPOTIFY_PUBLIC_USER_URL.format(username=request.auth.username)
--- api/profile_api/tests.py
+++ api/profile_api/tests.py
@@ -1,5 +1,13 @@
 import pytest
 from . import schemas
+from auth_api import jwt
+import auth_api
+from django.urls import reverse
+
+
+@pytest.fixture
+def route():
+    return reverse("api-1.0.0:profile")
 
 
 @pytest.fixture
@@ -31,3 +39,24 @@ def test_spotify_public_profile_schema(spotify_public_profile_response):
     assert user.external_urls.path == "/user/run2dos"
     assert user.external_urls.query == None
     assert user.followers == 1
+
+
+@pytest.mark.skip(reason="limit calls to spotify api.")
+def test_public_profile_route(db, client, route, django_user_model):
+    verified_user = auth_api.schemas.User(id=1, username="run2dos")
+    user = django_user_model.objects.create_user(
+        id=1, username="run2dos", email="run2dos@gmail.com"
+    )
+    token = jwt.create_access_token(verified_user=verified_user)
+    headers = {"HTTP_AUTHORIZATION": f"Bearer {token.access_token}"}
+    response = client.get(route, format="json", **headers)
+
+    assert route == "/api/profile"
+    assert response.json().get("username") == "run2dos"
+    assert response.json().get("display_name") == "Kevin"
+    assert response.json().get("country") == None
+    assert response.json().get("email") == None
+    assert response.json().get("premium") == None
+    assert response.json().get("followers") == 1
+    assert response.json().get("type") == "user"
+    assert response.json().get("uri") == "spotify:user:run2dos"
