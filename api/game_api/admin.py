20a7632e3e220f3007daf9063aa1f6922bca0998
dgnsrekt <dgnsrekt@pm.me>
2021-12-28 12:59:57 -0600

    feat: registered game models.

--- api/game_api/admin.py
+++ api/game_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Game, Stage, Choice, ScoreBoard
 
 # Register your models here.
+admin.register(Game)
+admin.register(Stage)
+admin.register(Choice)
+admin.register(ScoreBoard)
