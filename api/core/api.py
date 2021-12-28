27febed305b92e91a8b35cf9460442efd62c437a
dgnsrekt <dgnsrekt@pm.me>
2021-12-28 12:57:07 -0600

    feat: init play_api router.

--- api/core/api.py
+++ api/core/api.py
@@ -4,6 +4,7 @@ from auth_api.api import router as auth_router
 from auth_api.jwt import AuthBearer, InvalidToken
 from game_api.api import router as game_router
 from profile_api.api import router as profile_router
+from play_api.api import router as play_router
 
 api = NinjaAPI(
     title="TopsifyleAPI",
@@ -20,4 +21,5 @@ def on_invalid_token(request, exc):
 
 api.add_router("/auth", auth_router, tags=["Authentication"])
 api.add_router("/profile", profile_router, auth=AuthBearer(), tags=["Profile"])
-api.add_router("/game", game_router, tags=["Game"])
+api.add_router("/game", game_router, tags=["Game"])  # TODO: Add auth.
+api.add_router("/play", play_router, tags=["Play"])  # TODO: Add auth.
