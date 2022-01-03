29e33aa91e1e44d16d642d4a48d4e519206e7571
dgnsrekt <dgnsrekt@pm.me>
2022-01-03 17:42:02 -0600

    cors fix 2

--- api/core/settings.py
+++ api/core/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # django-cors-headers
 # https://github.com/adamchainz/django-cors-headers
 
-ORIGINS = ["134.122.30.228"]
+ORIGINS = ["http://134.122.30.228"]
 
 CORS_ALLOWED_ORIGINS = ORIGINS
 CSRF_TRUSTED_ORIGINS = ORIGINS
